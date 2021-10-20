77cdf35259d692c7c41c5a05da238629332e08c8
Daniel Kolev <danibanani.js@gmail.com>
2021-10-20 20:06:17 +0300

    Complete first exercise

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,17 +3,28 @@
 
 import * as React from 'react'
 
-function Counter({initialCount = 0, step = 1}) {
-  // 🐨 replace React.useState with React.useReducer.
-  // 💰 React.useReducer(countReducer, initialCount)
-  const [count, setCount] = React.useState(initialCount)
+const countReducer = (state, action) => {
+  const { type, step } = typeof action === 'function' ? action(state) : action
+  switch (type) {
+    case 'INCREMENT': 
+    return { count: state.count + step}
+    case 'DECREMENT':
+      return { count: state.count - step}
+    default:
+      throw new Error(`Unhandled action type ${type}`)
+  }
+}
 
-  // 💰 you can write the countReducer function so you don't have to make any
-  // changes to the next two lines of code! Remember:
-  // The 1st argument is called "state" - the current value of count
-  // The 2nd argument is called "newState" - the value passed to setCount
-  const increment = () => setCount(count + step)
-  return <button onClick={increment}>{count}</button>
+function Counter({initialCount = 0, step = 1}) {
+  const [state, setState] = React.useReducer(countReducer, {count: initialCount})
+
+  const increment = () => setState({ type: 'INCREMENT', step })
+  const decrement = () => setState({ type: 'DECREMENT', step })
+  return <>
+    <span>{state.count}</span>
+    <button onClick={increment}>+</button>
+    <button onClick={decrement}>-</button>
+  </>
 }
 
 function App() {
